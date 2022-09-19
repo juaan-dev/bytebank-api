c775a7e58ddf3adbe00c9fd564b0a81eb6078463
Juan Oliveira <17666@bemol.local>
2022-09-19 16:47:10 -0400

    Interface/Insert contacts and account number

---
+++ android/app/src/main/kotlin/br/alura/com/bytebank/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.alura.com.bytebank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,31 @@
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Contacts'),),
+      body: ListView(
+        children:[
+          Card(
+            child: ListTile(
+              title: Text('Miguel', style: TextStyle(fontSize: 24.0),),
+              subtitle: Text('2000', style: TextStyle(fontSize: 24.0),),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => ContactForm(),
+          ),
+          ).then((newContact) => debugPrint(newContact.toString()),
+          );
+        },
+        child: Icon(Icons.add,),
+      ),
+    );
+  }
+}
